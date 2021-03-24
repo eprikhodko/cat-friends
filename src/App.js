185619abb127e581591e2d8f3cf322fd4e125475
Evgeny Prikhodko <eprikhodkodev@gmail.com>
2021-03-24 23:39:18 +0300

    Pass props as object to ContactCard component

--- src/App.js
+++ src/App.js
@@ -6,16 +6,42 @@ import Header from "./Header"
 function App() {
   return (
     <div className="App">
+
       <Header />
 
       <ContactCard 
-        name="Mr. Whiskerson" 
-        imgUrl="http://placekitten.com/300/200" 
-        phone="(212) 555-1234" 
-        email="mr.whiskaz@catnap.meow"
+        contact={{
+          name: "Mr. Whiskerson", 
+          imgUrl: "http://placekitten.com/300/200",
+          phone: "(212) 555-1234",
+          email: "mr.whiskaz@catnap.meow"
+        }}
       />
 
-      <ContactCard />
+      <ContactCard 
+          contact={{
+            name: "Fluffykins", 
+            imgUrl: "http://placekitten.com/400/200", 
+            phone: "(212) 555-2345", 
+            email: "fluff@me.com"}}
+      />
+      
+      <ContactCard
+          contact={{
+            name: "Destroyer", 
+            imgUrl: "http://placekitten.com/400/300", 
+            phone: "(212) 555-3456", 
+            email: "ofworlds@yahoo.com"}}
+      />
+      
+      <ContactCard 
+          contact={{
+            name: "Felix", 
+            imgUrl: "http://placekitten.com/200/100", 
+            phone: "(212) 555-4567", 
+            email: "thecat@hotmail.com"}}
+      />
+
     </div>
   );
 }
