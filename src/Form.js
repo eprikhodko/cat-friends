0efbd733d97ea93df8fbee700a2da5824e67af8f
Evgeny Prikhodko <eprikhodkodev@gmail.com>
2021-03-29 12:59:29 +0300

    Convert class based Form component to functional component use useState hook for setting state.

--- src/Form.js
+++ src/Form.js
@@ -1,44 +1,44 @@
-import React, {Component} from "react"
+import React, {useState} from "react"
 
-class Form extends Component {
-    constructor() {
-        super()
-        this.state = {
-            name: "",
-            email: ""
-        }
-        this.handleChange = this.handleChange.bind(this)
-    }
-
-    handleChange(event) {
-        const {name, value} = event.target
-        this.setState({
-            [name]: value
-        })
-    }
+function Form() {
     
-    render() {
-        return (
-            <form className="form">
-                <input 
-                    type="text" 
-                    value={this.state.name}
-                    name="name" 
-                    placeholder="Name" 
-                    onChange={this.handleChange} 
-                />
+    const [inputData, setInputData] = useState({
+        name: "",
+        email: ""
+    })
 
-                <input 
-                    type="email"
-                    value={this.state.email} 
-                    name="email" 
-                    placeholder="email" 
-                    onChange={this.handleChange} 
-                />
-                <h2>{this.state.name} {this.state.email}</h2>
-            </form>
+    const [contactsData, setContactsData] = useState([])
+
+    function handleChange(event) {
+        
+    }
+
+    function handleSubmit(event) {
+
+    }
+
+        return (
+            <div className="container-form">
+                <form className="form">
+                    <input 
+                        type="text" 
+                        value={inputData.name}
+                        name="name" 
+                        placeholder="Name" 
+                        onChange={handleChange} 
+                    />
+
+                    <input 
+                        type="email"
+                        value={inputData.email} 
+                        name="email" 
+                        placeholder="email" 
+                        onChange={handleChange} 
+                    />
+                    <button>Add cat</button>
+                </form>
+            </div>
         )
     }
-}
 
 export default Form
