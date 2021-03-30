de0d3be3cd4cac6b2b646eef2556961ea1965d39
eprikhodko <eprikhodkodev@gmail.com>
2021-03-30 16:22:42 +0300

    Add form that create new cat cards components

--- src/Form.js
+++ src/Form.js
@@ -1,16 +1,19 @@
 import React, {useState} from "react"
+import ContactCard from "./ContactCard"
 
 function Form() {
     
     const [inputData, setInputData] = useState({
         name: "",
+        photo: "",
+        phone: "",
         email: ""
     })
 
     const [contactsData, setContactsData] = useState([])
 
     function handleChange(event) {
-        // destructuring 
+        //object destructuring 
         const {name, value} = event.target
         setInputData(prevInputData => {
             return {
@@ -21,17 +24,40 @@ function Form() {
     }
 
     function handleSubmit(event) {
-        // add the inputData to the contactsData array
+        event.preventDefault()
+        setContactsData(prevContacts => [...prevContacts, inputData])
     }
+
+
+    // here we're rendering new ContactCard components using contactsData array, which consists of objects that we're creating through form. We're passing {contact} object as props to ContactCard component.
+    const contactComponents = contactsData.map(contact => <ContactCard key={contact.name + contact.email} contactInfo={contact}/>)
+    // this is a contacts array using for showing form data inside of h2 elements. For debugging purposes only.
+    // const contacts = contactsData.map(contact => <h2 key={contact.name + contact.email}> {contact.name} {contact.imgUrl} </h2>)
 
         return (
             <div className="container-form">
-                <form className="form">
+                <form onSubmit={handleSubmit} className="form">
                     <input 
                         type="text" 
                         value={inputData.name}
                         name="name" 
                         placeholder="Name" 
+                        onChange={handleChange} 
+                    />
+
+                    <input 
+                        type="text"
+                        value={inputData.imgUrl} 
+                        name="imgUrl" 
+                        placeholder="photo url" 
+                        onChange={handleChange} 
+                    />
+
+                    <input 
+                        type="tel"
+                        value={inputData.phone} 
+                        name="phone" 
+                        placeholder="phone" 
                         onChange={handleChange} 
                     />
 
@@ -42,8 +68,25 @@ function Form() {
                         placeholder="email" 
                         onChange={handleChange} 
                     />
+
+                    <input 
+                        type="text"
+                        value={inputData.test} 
+                        name="test" 
+                        placeholder="test field for form" 
+                        onChange={handleChange} 
+                    />
+
+                     
                     <button>Add cat</button>
                 </form>
+
+
+                <div className="contact-components">
+                    {contactComponents}
+                </div>
+
+                {/* {contacts} */}
             </div>
         )
     }
